0a2a2314c79459c36fd5f6d6e3b8e1b05301aa54
jegavni <jegatheeshchandra@gmail.com>
2026-03-12 15:35:36 +0530

    FILe

--- Services/Gateway/src/server.js
+++ Services/Gateway/src/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://ktm-vg51.onrender.com"],
     credentials: true
   })
 );
