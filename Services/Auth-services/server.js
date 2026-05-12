eb6b2434000de7248b56364b601a415f6531b0bd
jegavni <jegatheeshchandra@gmail.com>
2026-05-12 11:53:09 +0530

    port fix

--- Services/Auth-services/server.js
+++ Services/Auth-services/server.js
@@ -33,6 +33,8 @@ app.use("/login", (req, res, next) => {
 
 app.use("/", authRoutes);
 
-app.listen(5001, () => {
-  console.log("Auth service running on port 5001");
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Auth service running on port ${PORT}`);
 });
